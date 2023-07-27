439c9df0cdf155ba775809cbeeafe851c0ad9a36
Maryam-Inam <maryaminam00@gmail.com>
2023-07-27 16:50:59 -0400

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+      "www.myfitnesspal.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
